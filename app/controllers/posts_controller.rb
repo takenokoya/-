12feae252ca62cf32b1c829b9c401633e05c3e6a
takenokoya <takuya1002@gmail.com>
2019-03-10 16:45:21 +0900

    ブログアプリ完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result.page(params[:page]).per(2)
+    @new_posts = Post.find_newest_article
     @author = Author.first
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -20,17 +21,14 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -39,4 +37,9 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body, :category)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
